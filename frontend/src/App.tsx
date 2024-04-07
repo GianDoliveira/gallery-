669001aae9b40a69af4bc67cc2401be2f8b31b74
Gian de Oliveira <naigdeoliveira@gmail.com>
2024-04-07 16:29:27 -0300

    login/register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import { UserProvider } from './Context/useAuth.tsx'
 import FormsComponent from './components/pages/forms/Forms.tsx'
 
 
 function App() {
-  return <FormsComponent/>
+  return (
+    <UserProvider>
+      <FormsComponent/>
+    </UserProvider>
+  )
 }
 
 export default App
--- frontend/src/components/pages/forms/SignUp.tsx
+++ frontend/src/components/pages/forms/SignUp.tsx
@@ -1,16 +1,71 @@
-const SignUpComponent = () => {
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useAuth } from "../../../Context/useAuth";
+import { useForm } from "react-hook-form";
+
+type Props = {};
+
+type RegisterFormsInputs = {
+    email: string;
+    userName: string;
+    password: string;
+};
+
+const validation = Yup.object().shape({
+    email: Yup.string().required("Email is required"),
+    userName: Yup.string().required("Username is required"),
+    password: Yup.string().required("Password is required"),
+});
+
+const SignUpComponent = (props: Props) => {
+
+    const { registerUser } = useAuth();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<RegisterFormsInputs>({ resolver: yupResolver(validation) });
+
+    const handleLogin = (form: RegisterFormsInputs) => {
+        registerUser(form.email, form.userName, form.password);
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit(handleLogin)}>
             <h1>Cadastre-se</h1>
             <input
-                type="text" placeholder="Nome de usuário" required
+                type="text"
+                placeholder="Nome de usuário"
+                required
+                {...register("userName")}
             />
+            {errors.userName ? (
+                <p className="text-white">{errors.userName.message}</p>
+            ) : (
+                ""
+            )}
             <input
-                type="email" placeholder="E-mail" required
+                type="email"
+                placeholder="E-mail"
+                required
+                {...register("email")}
             />
+            {errors.email ? (
+                <p className="text-white">{errors.email.message}</p>
+            ) : (
+                ""
+            )}
             <input
-                type="password" placeholder="Senha" required
+                type="password"
+                placeholder="Senha"
+                required
+                {...register("password")}
             />
+            {errors.password ? (
+                <p className="text-white">{errors.password.message}</p>
+            ) : (
+                ""
+            )}
             <button type='submit'>Cadastrar</button>
         </form>
     )
